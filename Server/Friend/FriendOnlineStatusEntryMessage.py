74570488e33610f6cbde8bec1e2b9fcf8138aa58
DEVELOPERcreation <168726830+DEVELOPERcreatinon@users.noreply.github.com>
2024-12-12 10:04:07 +0900

    fixed bug

--- Server/Friend/FriendOnlineStatusEntryMessage.py
+++ Server/Friend/FriendOnlineStatusEntryMessage.py
@@ -1,50 +1,24 @@
-т"
-			},
-			"nl": {
-				"name": "Taal-specifieke"
-			},
-			"no": {
-				"name": "Språkspesifikke"
-			},
-			"pl": {
-				"name": "Specyficzne językowe"
-			},
-			"pt": {
-				"name": "De um idioma específico"
-			},
-			"pt_BR": {
-				"name": "Específico por idioma"
-			},
-			"pt_PT": {
-				"name": "De um idioma específico"
-			},
-			"ro": {
-				"name": "Limba specifică"
-			},
-			"ru": {
-				"name": "Языковые"
-			},
-			"sk": {
-				"name": "Jazykovo špecifické"
-			},
-			"sl": {
-				"name": "Specifično za jezik"
-			},
-			"sr": {
-				"name": "Poseban jezički"
-			},
-			"sv": {
-				"name": "Språkspecifika"
-			},
-			"tr": {
-				"name": "Dile Özgü"
-			},
-			"uk": {
-				"name": "Для певної мови"
-			},
-			"vi": {
-				"name": "Ngôn ngữ cụ thể"
-			},
-			"zh": {
-				"name": "特定语言"
-	
+from Utils.Writer import Writer
+
+class FriendOnlineStatusEntryMessage(Writer):
+
+    def __init__(self, client, player, lowID, status, room_id):
+        super().__init__(client)
+        self.id = 24555
+        self.player = player 
+        self.lowID = lowID
+        self.status = status
+        self.room_id = room_id
+    def encode(self):
+        self.writeInt(0)
+        self.writeInt(self.lowID)#лов ид
+        self.writeBoolean(True)
+        self.writeInt(0)
+        self.writeInt(self.lowID)#лов ид
+        self.writeVint(self.status)#стаиус
+        self.writeVint(0)#если вверху 0 то тут ставь 1 и будет он не в сетм
+        if self.room_id > 0:
+            self.writeBoolean(True)
+        else:
+            self.writeBoolean(False)
+        self.writeBoolean(False)	
